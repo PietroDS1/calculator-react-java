957d603bf377272a75ea6b3914a62798e81125bf
Marllon Campos <marllondcsp@gmail.com>
2023-05-22 17:09:41 -0300

    feat: create a validator from write on visor to just allow one operation at time

--- src/components/Body/index.tsx
+++ src/components/Body/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { Button } from "../Button"
 import { Visor } from "../Visor"
 
@@ -5,12 +6,21 @@ const NUMPAD_SYMBOLS = ["AC","+/-", "%","÷","7","8","9","X", "4","5","6","-","1
 
 
 export const Body = () => {
+    const [visorValue, setVisorValue] = useState("")
+    function writeOnVisor(value: string) {
+        if(/\D/g.test(visorValue) && /\D/g.test(value)) return alert('Só podemos realizar uma operação por vez v.1')
+       setVisorValue(prevState => `${prevState}${value}`)
+    }
     return (
         <section className="body">
+            <p>{visorValue}</p>
             <Visor />
             <div className="numpad">
             {NUMPAD_SYMBOLS.map((value,index) => (
-                <Button label={value} value={value} data-aria-value={value} key={index} />
+                <Button label={value} value={value} 
+                    data-aria-value={value} key={index}
+                    onClick={() => writeOnVisor(value)} 
+                />
             ))}
                 
             </div>
